ef1024f54d5413ea60d1e4790525713460d97a49
Hiroaki-Kaga <strawberrytable451@yahoo.co.jp>
2020-02-01 12:36:28 +0000

    Laravel03

--- Laravel03.php
+++ Laravel03.php
@@ -7,5 +7,32 @@ if($name == "Hiroaki"){
     echo $name."ではありません";
 }
 
+$total = 0;
+for ($i = 1; $i <= 10000; $i++) {
+     $total += $i;
 
-1234567890
+}
+echo $total.;
+
+$fruits = ("orange", "apple", "strawberry", "peach", "none",)
+foreach ($fruits as $fruit) {
+    echo  $fruit.;
+}
+
+
+$start =1;
+$end = 100;
+
+for($i = $start; $i <= $end; $i++) {
+
+  if($i % 5 == 0){
+    echo $i;
+  }
+
+}
+
+
+
+
+
+
